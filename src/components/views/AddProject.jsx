d712d25372a4485654dd33b8e7ddfd49ab231c6f
JennaKoski <jenna.m.koski@gmail.com>
2022-10-28 15:44:54 +0300

    // eslint-disable-next-line added to duid.js, AddProject styling done

--- src/components/views/AddProject.jsx
+++ src/components/views/AddProject.jsx
@@ -1,53 +1,56 @@
-import React from 'react';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Project } from '../../classes/project';
-import { addProject, setProject } from '../../features/druidSlice';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Project } from "../../classes/project";
+import { addProject, setProject } from "../../features/druidSlice";
 
-import { capitalStart } from '../../functions/capitalStart';
-
+import { capitalStart } from "../../functions/capitalStart";
 
 //service
-import { postProject } from '../../services/druid';
+import { postProject } from "../../services/druid";
 
 const AddProject = () => {
-    const dispatch = useDispatch()
-    const project = useSelector(state => state.druid.project) 
+  const dispatch = useDispatch();
+  const project = useSelector((state) => state.druid.project);
 
-    const changeData = (e) => {
-        dispatch(setProject({...project, [e.target.name]: capitalStart(e.target.value)}))
-      };
+  const changeData = (e) => {
+    dispatch(
+      setProject({ ...project, [e.target.name]: capitalStart(e.target.value) })
+    );
+  };
 
-     const createProject = (e) => {
-        e.preventDefault();
-        if(project.name && project.client){
-            dispatch(addProject(project));
-            const projectObj = new Project(project.name, project.client);
-            postProject(projectObj);
-        }else{
-            console.log("missing info in the project")
-        }
-     }
+  const createProject = (e) => {
+    e.preventDefault();
+    if (project.name && project.client) {
+      dispatch(addProject(project));
+      const projectObj = new Project(project.name, project.client);
+      postProject(projectObj);
+    } else {
+      console.log("missing info in the project");
+    }
+  };
 
   return (
-    <div>
-        <p>AddProject</p>
-        <form onSubmit={createProject}>
-
-            <div >
-              <label htmlFor="name">Name </label>
-              <input type="text" name="name" id="name" onChange={changeData} />
-            </div>
-
-            <div >
-              <label htmlFor="client">Client </label>
-              <input type="text" name="client" id="name" onChange={changeData} />
-            </div>
-            <input type="submit" value="create project" />
-        </form>
-
+    <div className="addProject">
+      <h3>Create new project</h3>
+      <form className="addProjectForm" onSubmit={createProject}>
+        <div className="addProjectInputs">
+          <div className="projectNameInput">
+            <label htmlFor="name">Project name </label>
+            <input type="text" name="name" id="name" onChange={changeData} />
+          </div>
+          <div className="customerInput">
+            <label htmlFor="client">Customer company </label>
+            <input type="text" name="client" id="name" onChange={changeData} />
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Create project"
+          className="createProjectButton"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProject
+export default AddProject;
--- src/services/druid.js
+++ src/services/druid.js
@@ -1,32 +1,40 @@
-import axios from 'axios';
-import { genId } from '../functions/genId';
+import axios from "axios";
+import { genId } from "../functions/genId";
 
-  const baseUrl = 'http://localhost:8010/database';
-  
-  const getDatabase = async () => {
-    const response = await axios.get(baseUrl);
-    return response.data;
+const baseUrl = "http://localhost:8010/database";
+
+const getDatabase = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+// eslint-disable-next-line
+export default { getDatabase };
+
+export const postProject = async (newProject) => {
+  const database = await getDatabase();
+  const toPostProject = {
+    ...newProject,
+    id: database.projects.length + 1,
+    code: genId(),
   };
-  
-  export default { getDatabase };
-
-  export const postProject = async (newProject) => {
-
-    
-    const database = await getDatabase();
-    const toPostProject = {...newProject, "id": database.projects.length + 1, "code": genId()}
-    const newDatabase = {...database, "projects": [ ...database.projects, toPostProject]}
-    const response = await axios.patch(baseUrl, newDatabase)
+  const newDatabase = {
+    ...database,
+    projects: [...database.projects, toPostProject],
+  };
+  const response = await axios
+    .patch(baseUrl, newDatabase)
     .then((response) => console.log(response))
     .catch((err) => console.log(err));
-    return response;
-  }
+  return response;
+};
 
-  export const postUser = async (newUser) => {
-    const database = await getDatabase();
-    const newDatabase = {...database, "users": [ ...database.projects, newUser]}
-    const response = await axios.patch(baseUrl, newDatabase)
+export const postUser = async (newUser) => {
+  const database = await getDatabase();
+  const newDatabase = { ...database, users: [...database.projects, newUser] };
+  const response = await axios
+    .patch(baseUrl, newDatabase)
     .then((response) => console.log(response))
     .catch((err) => console.log(err));
-    return response;
-  }
+  return response;
+};
