18209c63d6d69f0fd4a6cc53a198aa4624db5b10
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-31 10:50:20 +0200

    added attributes to cals project

--- src/classes/project.js
+++ src/classes/project.js
@@ -3,7 +3,27 @@ export class Project {
     code = undefined
     developers = []
     status = undefined //progress, complete, pending
+    //Service	Engine	Version	PHP	JS	Node	Drush	Omen	DBS	Mails	Search	CDN	Infra	Docker	Hosting	Deps	CI	dev & main
 
+    service = []
+    engine = []
+    version = []
+    php = []
+    js = []
+    node = []
+    drush = []
+    omen = []
+    dbs = []
+    mails = []
+    search = []
+    cdn = []
+    infra = []
+    docker = []
+    hosting = []
+    deps = []
+    ci = []
+    dev_n_main = []
+    
     constructor(name, client ){
         this.name = name
         this.client = client
