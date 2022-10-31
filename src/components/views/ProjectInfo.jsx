381474a1e78741dd578a2e9d220c4c31a0c8d138
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-31 10:40:25 +0200

    th tr problem solved

--- src/components/views/ProjectInfo.jsx
+++ src/components/views/ProjectInfo.jsx
@@ -6,75 +6,72 @@ const ProjectInfo = () => {
       <div className="projectInfo">
         <h3>Project name</h3>
         <table className="table">
-          <tr>
-            <th>Service</th>
-            <th>Engine</th>
-            <th>Version</th>
-            <th>PHP</th>
-            <th>JS</th>
-            <th>Node</th>
-            <th>Drush</th>
-            <th>Omen</th>
-            <th>DBS</th>
-            <th>Mails</th>
-            <th>Search</th>
-          </tr>
-          <tr>
-            <td>url1</td>
-            <td>Drupal</td>
-            <td>9.4</td>
-            <td>8.0</td>
-            <td>16</td>
-            <td>webpack</td>
-            <td>Mailjet</td>
-            <td>Solr 8</td>
-            <td>11</td>
-            <td>x</td>
-            <td>MariaDB 10.5</td>
-          </tr>
-          <tr>
-            <td>url2</td>
-            <td>Drupal</td>
-            <td>8.9</td>
-            <td>7.3</td>
-            <td>8</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-          </tr>
-        </table>
-        <table className="table">
-          <tr>
-            <th>CDN</th>
-            <th>Infra</th>
-            <th>Docker</th>
-            <th>Hosting</th>
-            <th>Deps</th>
-            <th>CI</th>
-            <th>dev & main</th>
-          </tr>
-          <tr>
-            <td>CloudFront</td>
+          <thead>
+            <tr>
+              <th>Service</th>
+              <th>Engine</th>
+              <th>Version</th>
+              <th>PHP</th>
+              <th>JS</th>
+              <th>Node</th>
+              <th>Drush</th>
+              <th>Omen</th>
+              <th>DBS</th>
+              <th>Mails</th>
+              <th>Search</th>
+              <th>CDN</th>
+              <th>Infra</th>
+              <th>Docker</th>
+              <th>Hosting</th>
+              <th>Deps</th>
+              <th>CI</th>
+              <th>dev & main</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>url1</td>
+              <td>Drupal</td>
+              <td>9.4</td>
+              <td>8.0</td>
+              <td>16</td>
+              <td>webpack</td>
+              <td>Mailjet</td>
+              <td>Solr 8</td>
+              <td>11</td>
+              <td>x</td>
+              <td>MariaDB 10.5</td>
+                          <td>CloudFront</td>
             <td>-</td>
             <td>uselagoon</td>
             <td>Lagoon</td>
             <td>Renovate</td>
             <td>GHA</td>
             <td>x</td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
+              <td>url2</td>
+              <td>Drupal</td>
+              <td>8.9</td>
+              <td>7.3</td>
+              <td>8</td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+                          <td>x</td>
             <td>x</td>
             <td>x</td>
             <td>x</td>
             <td>x</td>
             <td>x</td>
             <td>x</td>
-            <td>x</td>
-          </tr>
+            </tr>
+          </tbody>
         </table>
+
       </div>
     </div>
   );
