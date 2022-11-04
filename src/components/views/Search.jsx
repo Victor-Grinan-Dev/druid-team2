99c01887119af615cdaa4bc1f7dc191029ad4fc1
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-04 20:34:14 +0200

    cleaned code

--- src/components/views/Search.jsx
+++ src/components/views/Search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 //redux
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setSearch, setSearchBy } from "../../features/druidSlice";
 
 const Search = () => {
