27e3cfb15a87b19ca72dd534f7d91d768e2db19c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-04 20:33:56 +0200

    cleaned code

--- src/components/views/Search.jsx
+++ src/components/views/Search.jsx
@@ -6,9 +6,7 @@ import { setSearch, setSearchBy } from "../../features/druidSlice";
 
 const Search = () => {
   const dispatch = useDispatch();
-  const search = useSelector(state => state.druid.search);
-  const searchBy = useSelector(state => state.druid.searchBy);
-  
+ 
   const handleSearch = (e) => {
     dispatch(setSearch(e.target.value))
   }
@@ -24,7 +22,6 @@ const Search = () => {
         <option value="project">by project</option>
         <option value="engine">by engine</option>
       </select>
-      <p>by {searchBy}, {search}</p>
     </div>
   );
 };
