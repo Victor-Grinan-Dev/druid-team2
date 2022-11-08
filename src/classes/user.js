da5bafd3c0f4407ee6203d437bf6c4f225ea7561
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-08 11:25:44 +0200

    created set initials function in user calss

--- src/classes/user.js
+++ src/classes/user.js
@@ -2,11 +2,22 @@ export class User{
     id = undefined;
     email = undefined;
     company = undefined;
-    firstName = undefined;
-    lastName = undefined;
-    //password
-    constructor(username, userType ){
+    initials = undefined;
+
+    constructor(username, userType, firstName=undefined, lastName=undefined ){
         this.username = username;
-        this.userType = userType;  
+        this.userType = userType; 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.setInitials();
+    }
+
+    setInitials(){
+        if(this.firstName && this.lastName){
+            this.initials = [firstName.splice(0)[0].toUpperCase(), lastName.splice(0)[0].toUpperCase()].join()
+        }
+        else {
+            this.initials = username.splice(2)[0];
+        }
     }
 }
