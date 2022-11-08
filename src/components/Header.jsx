00113f0251a13a1de07266a1bfad6b3ea1218790
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-08 10:14:29 +0200

    added logo link to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 import image from "../assets/images/druid.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={image} alt="Druid logo" className="logo" />
+      <Link to="/"><img src={image} alt="Druid logo" className="logo" /></Link>
       <div>
         <Navbar />
       </div>
