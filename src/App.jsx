ec0783cd377c84da434a1bc14aeb690e53f2d88e
JennaKoski <jenna.m.koski@gmail.com>
2022-11-15 10:01:11 +0200

    changes in ajax folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ import Cookies from "js-cookie";
 
 //redux
 import { useDispatch, useSelector } from "react-redux";
-import { isLoading, setConfig, setIsLogged, setProjects, setUser, setUsers } from "./features/druidSlice";
-import druidService from './services/druid';
+import {
+  isLoading,
+  setConfig,
+  setIsLogged,
+  setProjects,
+  setUser,
+  setUsers,
+} from "./features/druidSlice";
+import druidService from "./services/druid";
 import { NodeList } from "./ajax/NodeList";
 
 //components
@@ -19,11 +26,10 @@ import Home from "./components/Home";
 import Users from "./components/views/Users";
 import Profile from "./components/views/Profile";
 
-
 function App() {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.druid.user);
- 
+  const user = useSelector((state) => state.druid.user);
+
   useEffect(() => {
     druidService.getDatabase().then((res) => {
       const projects = res.projects;
@@ -34,34 +40,32 @@ function App() {
       dispatch(setUsers(users));
       dispatch(setConfig(config));
 
-      if(Cookies.get("druidLog")){
-        const cookie = Cookies.get("druidLog")
-        for (let user of users){
-          if(user.id === cookie){
-            dispatch(setUser(user))
-            dispatch(setIsLogged(true))
+      if (Cookies.get("druidLog")) {
+        const cookie = Cookies.get("druidLog");
+        for (let user of users) {
+          if (user.id === cookie) {
+            dispatch(setUser(user));
+            dispatch(setIsLogged(true));
           }
         }
       }
     });
-    
+
     dispatch(isLoading(false));
   }, [dispatch]);
 
   const views = () => {
     return (
       <>
-          <Route path="customersprojects" element={<CustomersProjects />} />
-          <Route path="projectinfo/:name" element={<ProjectInfo />} />
-          {/* <Route path="userinfo/:name" element={<Profile />} /> */}
-          <Route path="addproject" element={<AddProject />} />
-          <Route path="adduser" element={<AddUser />} />
-          <Route path="users" element={<Users />} />
-          <Route path="profile" element={<Profile />} />
-
+        <Route path="customersprojects" element={<CustomersProjects />} />
+        <Route path="projectinfo/:name" element={<ProjectInfo />} />
+        {/* <Route path="userinfo/:name" element={<Profile />} /> */}
+        <Route path="addproject" element={<AddProject />} />
+        <Route path="adduser" element={<AddUser />} />
+        <Route path="users" element={<Users />} />
+        <Route path="profile" element={<Profile />} />
       </>
-    )
-    
+    );
   };
   return (
     <BrowserRouter>
@@ -70,12 +74,11 @@ function App() {
           <Route index element={<Home />} />
           {user.username && views()}
         </Route>
-        
       </Routes>
-
-      <NodeList /> 
+      <div>
+        <NodeList />
+      </div>
       {/* <NodeForm /> */}
-      
     </BrowserRouter>
   );
 }
