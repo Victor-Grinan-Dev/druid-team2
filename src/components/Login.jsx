558e01dfc2814d3aaa30b335487aab9b646e8823
JennaKoski <jenna.m.koski@gmail.com>
2022-11-17 11:05:18 +0200

    login changed to drupal db

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,67 +3,78 @@ import Cookies from "js-cookie";
 import React, { useRef, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import logo from "../assets/images/logo.jpg";
+import config from "../ajax/config";
+import { User } from "../classes/user";
 
 //Auth:
 //import AuthContext from "../context/AuthProvider";
 import { setIsLogged, setUser } from "../features/druidSlice";
 
-const LOGIN_URL = 'http://localhost:8010/database'; //auth
+// const LOGIN_URL = "http://localhost:8010/database"; //auth
 
 const Login = () => {
   const dispatch = useDispatch();
   //const isLogged = (state => state.druid.isLogged);
 
-  const [errMsg, setErrMsg] = useState('');
+  const [errMsg, setErrMsg] = useState("");
   const userRef = useRef();
   const errRef = useRef();
 
-  const [pwd, setPwd] = useState('');
-  const [user, setUsername] = useState('');
+  const [pwd, setPwd] = useState("");
+  const [user, setUsername] = useState("");
   //const { setAuth } = useContext(AuthContext);
 
-  useEffect(()=>{
+  useEffect(() => {
     //userRef.current.focus();
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-      setErrMsg('');
-  },[user, pwd]);
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    
-    try{
-        const response = await axios.get(LOGIN_URL)
-        const arr = response.data.users;
+    e.preventDefault();
 
-        arr.forEach(element => {
-          if (element.username.toLowerCase() === user.toLowerCase() && element.pwd === pwd){
- 
-            localStorage.setItem("druid", JSON.stringify(element));
-            dispatch(setIsLogged(true));
-            dispatch(setUser(element));
-            Cookies.set("druidLog", element.id )
-          } else {
-            setErrMsg('Wrong Email or Password!');
-          }
+    try {
+      // const response = await axios.get(LOGIN_URL);
+      // const arr = response.data.users;
+
+      // arr.forEach((element) => {
+      //   if (
+      //     element.username.toLowerCase() === user.toLowerCase() &&
+      //     element.pwd === pwd
+      //   )  else {
+      //     setErrMsg("Wrong Email or Password!");
+      //   }
+      // });
+
+      await axios
+        .post(`${config.drupal_url}/user/login?_format=json`, {
+          name: user,
+          pass: pwd,
+        })
+        .then((res) => {
+          Cookies.set("druidLog", res.data.csrf_token);
+          const testUser = new User(res.data.current_user.name, "pm");
+          localStorage.setItem("druid", JSON.stringify(testUser));
+          dispatch(setIsLogged(true));
+          dispatch(setUser(testUser));
+          console.log(res);
         });
-
-
-    } catch (err){
-        if(!err?.response){
-            setErrMsg('No server response');
-        } else if (err.response?.status === 400){
-            setErrMsg('Missing Username or password');
-        } else if (err.response?.status === 401){
-            setErrMsg('Unauthorized');
-        } else {
-            setErrMsg('Login failed');
-        }
-        console.log(err)
-        errRef.current.focus();
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No server response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login failed");
+      }
+      console.log(err);
+      errRef.current.focus();
     }
-}
+  };
   return (
     <div className="login">
       <div className="loginLeftSide">
@@ -74,23 +85,31 @@ const Login = () => {
           answers to your questions.
         </p>
       </div>
-      <form onSubmit={handleSubmit} >
-      <div className="loginRightSide">
-        <div className="username">
-          <label htmlFor="username">Username</label>
-          <input ref={userRef} type="text" placeholder="Enter username" onChange={(e)=>setUsername(e.target.value)}/>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <div className="loginRightSide">
+          <div className="username">
+            <label htmlFor="username">Username</label>
+            <input
+              ref={userRef}
+              type="text"
+              placeholder="Enter username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
 
-        <div className="password">
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Enter password" onChange={(e)=>setPwd(e.target.value)} />
-        </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              onChange={(e) => setPwd(e.target.value)}
+            />
+          </div>
 
-        <button className="loginButton">Log in</button>
-      </div>
-      <div ref={errRef}>{errMsg}</div>
+          <button className="loginButton">Log in</button>
+        </div>
+        <div ref={errRef}>{errMsg}</div>
       </form>
-      
     </div>
   );
 };
