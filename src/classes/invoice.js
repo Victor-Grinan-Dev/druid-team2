94c8c80d075355f030e37fa8bb03d978a2e4c576
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-21 12:58:52 +0200

    invoices are being called ok

---
+++ src/classes/invoice.js
@@ -0,0 +1,18 @@
+export class Invoice{ //OOP
+    
+    isActive = false;
+    isSent = false;
+    isPaid = false;
+    createdDateTime = undefined;
+    
+    constructor( guid, name, createdBy){
+        this.guid = guid;
+        this.name = name;
+        this.createdBy = createdBy;
+        this.setCreatedTime();
+    }
+    setCreatedTime(){
+        this.createdDateTime = new Date();
+    }
+};
+
