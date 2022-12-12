1920f627dd558bfb081f00a99f577cd4051551f9
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-12 14:53:39 +0200

    prjects map updated

--- src/components/views/CustomersProjects.jsx
+++ src/components/views/CustomersProjects.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import events from "events";
+import ajax from "../../ajax/ajax";
 //import ProjectBox from "./ProjectBox";
 import { useSelector } from "react-redux";
 import Search from "./Search";
 import ProjectCard from "./projectCard/ProjectCard";
-import { Projects } from "../../ajax/Projects";
+//import { Projects } from "../../ajax/Projects";
+
+const emitter = new events.EventEmitter();
 
 const CustomersProjects = () => {
-  const projects = useSelector((state) => state.druid.projects);
-  const isLoading = useSelector((state) => state.druid.isLoading);
-  const user = useSelector((state) => state.druid.user);
-  const search = useSelector((state) => state.druid.search);
-  const searchBy = useSelector((state) => state.druid.searchBy);
+  //const projects = useSelector((state) => state.druid.projects);
+  //const isLoading = useSelector((state) => state.druid.isLoading);
+  //const user = useSelector((state) => state.druid.user);
+  //const search = useSelector((state) => state.druid.search);
+  //const searchBy = useSelector((state) => state.druid.searchBy);
 
+  const [projects, setProjects] = useState()
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+   const getProjects = async () => {
+    try {
+      const axios = await ajax();
+      const response = await axios.get("/node/osproject2");
+      console.log("projects:", response.data)
+      if (response.data) {
+        setProjects(response.data);
+      }
+    } catch (e) {
+      alert(e);
+    }
+  }
+/*
   const filteredProjectsHandler = () => {
     switch (searchBy) {
       case "customer":
@@ -53,7 +76,9 @@ const CustomersProjects = () => {
         return projects;
     }
   };
+*/
 
+/*
   const access = () => {
     if (user.userType === "customer") {
       return projects.map(
@@ -70,11 +95,13 @@ const CustomersProjects = () => {
           )
       );
     }
-
-    return filteredProjectsHandler().map((project, i) => (
+        return filteredProjectsHandler().map((project, i) => (
       <ProjectCard project={project} index={i + 1} key={i} full={false} />
     ));
   };
+*/
+
+
 
   /*
   if (isLoading) {
@@ -91,8 +118,19 @@ const CustomersProjects = () => {
       </div>
       <h2 className="projectsH2">Projects</h2>
       <div className="cardsArea">
-        <Projects />
-        {/* acces() */}
+        {
+          projects &&
+          projects.map((project, index) => {
+            return (
+              <ProjectCard
+                key={index}
+                nid={project.nid[0].value}
+                project={project}
+              />
+            );
+          })}
+        {/* acces()
+        <Projects /> */}
       </div>
     </div>
   );
