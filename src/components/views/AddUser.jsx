086c7bc48ebcc51f65460eefe3da193e061b4b0e
JennaKoski <jenna.m.koski@gmail.com>
2022-12-14 18:13:18 +0200

    stylings fixed

--- src/components/views/AddUser.jsx
+++ src/components/views/AddUser.jsx
@@ -13,7 +13,7 @@ const emitter = new events.EventEmitter();
 const AddUser = () => {
   const [expand, setExpand] = useState(false);
   const [customers, setCustomers] = useState();
-  const [data, setData] = useState({status: [{ value: true }]});
+  const [data, setData] = useState({ status: [{ value: true }] });
 
   useEffect(() => {
     getCustomers();
@@ -21,39 +21,39 @@ const AddUser = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      console.log(data);
-        try {
-          const axios = await ajax();
-          const response = await axios.post("/entity/user", data);
-          console.log("Node created: ", response.data);
-          emitter.emit("NODE_UPDATED");
-        } catch (e) {
-          alert(e);
-        }
-    setData({status: [{ value: true }]});
+    console.log(data);
+    try {
+      const axios = await ajax();
+      const response = await axios.post("/entity/user", data);
+      console.log("Node created: ", response.data);
+      emitter.emit("NODE_UPDATED");
+    } catch (e) {
+      alert(e);
+    }
+    setData({ status: [{ value: true }] });
   };
 
   const handleChange = (e, propName) => {
-    if(propName === "field_company" || propName === "roles"){
-      setData({...data, [propName]:[{ "target_id": e.target.value}]});
-    }else{
-      setData({...data, [propName]:[{ "value": e.target.value}]});
+    if (propName === "field_company" || propName === "roles") {
+      setData({ ...data, [propName]: [{ target_id: e.target.value }] });
+    } else {
+      setData({ ...data, [propName]: [{ value: e.target.value }] });
     }
-    
-    if(propName === 'roles'){
-      if(e.target.value === "customer_user"){
+
+    if (propName === "roles") {
+      if (e.target.value === "customer_user") {
         setExpand(true);
-      }else{
+      } else {
         setExpand(false);
       }
     }
   };
-  
+
   const getCustomers = () => {
-    ajaxGet('/node/customers').then(res => {
-      setCustomers(res)
-    })
-  }
+    ajaxGet("/node/customers").then((res) => {
+      setCustomers(res);
+    });
+  };
 
   return (
     <div>
@@ -61,56 +61,71 @@ const AddUser = () => {
       <form onSubmit={handleSubmit}>
         <div className="createUserContainer">
           <div>
-          <label className="createUserLabels">
-              {" "}
-              Username:{" "}
-            </label>
-            <input type="text" onChange={(e) => handleChange(e, "name")} className="createUserInputs"></input>
+            <label className="createUserLabels"> Username: </label>
+            <input
+              type="text"
+              onChange={(e) => handleChange(e, "name")}
+              className="createUserInputs"
+            ></input>
           </div>
           <div>
             <label htmlFor="email" className="createUserLabels">
               {" "}
               Email:{" "}
             </label>
-            <input type="text" onChange={(e) => handleChange(e, "mail")} className="createUserInputs" />
+            <input
+              type="text"
+              onChange={(e) => handleChange(e, "mail")}
+              className="createUserInputs"
+            />
           </div>
-            <div>
-            <label className="createUserLabels">Password</label>
-  
-        <input type="text" onChange={(e) => handleChange(e, "pass")} className="createUserInputs"/>
-            </div>
+          <div>
+            <label className="createUserLabels">Password:</label>
+
+            <input
+              type="text"
+              onChange={(e) => handleChange(e, "pass")}
+              className="createUserInputs"
+            />
+          </div>
           <div>
             <label htmlFor="customer" className="createUserLabels">
               {" "}
               User type:{" "}
             </label>
-            <select onChange={(e) => handleChange(e, "roles")} className="createUserInputs">
-              <option value="" hidden>Choose...</option>
-              <option value="developer" >Developer</option>
-              <option value="customer_user" >Customer user</option>
-        </select>
+            <select
+              onChange={(e) => handleChange(e, "roles")}
+              className="createUserInputs"
+            >
+              <option value="" hidden>
+                Choose...
+              </option>
+              <option value="developer">Developer</option>
+              <option value="customer_user">Customer user</option>
+            </select>
           </div>
 
-          {
-            expand && 
-
+          {expand && (
             <div>
               <label htmlFor="company" className="createUserLabels">
-   
                 Company:
               </label>
-              <select onChange={(e) => handleChange(e, "field_company")} className="createUserInputs">
-              <option value="" hidden>Choose...</option>
-                {
-                  customers && customers.map((c, i) => (
-        
-                    <option key={i} value={parseInt(c.nid[0].value, 10)}>{c.title[0].value} - {c.nid[0].value}</option>
-                  ))
-                }
-        </select>
+              <select
+                onChange={(e) => handleChange(e, "field_company")}
+                className="createUserInputs"
+              >
+                <option value="" hidden>
+                  Choose...
+                </option>
+                {customers &&
+                  customers.map((c, i) => (
+                    <option key={i} value={parseInt(c.nid[0].value, 10)}>
+                      {c.title[0].value} - {c.nid[0].value}
+                    </option>
+                  ))}
+              </select>
             </div>
-            
-          }
+          )}
           <input
             type="submit"
             value="Create user"
--- src/components/views/projectCard/SingleProjectCard.jsx
+++ src/components/views/projectCard/SingleProjectCard.jsx
@@ -1,95 +1,94 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import ajax from '../../../ajax/ajax';
-import { ajaxGet, ajaxPost } from '../../../ajax/services';
-import { capitalStart } from '../../../functions/capitalStart';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { ajaxGet } from "../../../ajax/services";
+import { capitalStart } from "../../../functions/capitalStart";
 
 const servicesKey = [
-    "field_hosting",
-    "field_cdn",
-    "field_ci_cd",
-    "field_cms",
-    "field_database",
-    "field_deps",
-    "field_docker",
-    "field_engine",
-    "field_framework",
-    "field_infra",
-    "field_language",
-    "field_libraries",
-    "field_mailing",
-    "field_search",
-  ]
+  "field_cms",
+  "field_engine",
+  "field_language",
+  "field_framework",
+  "field_libraries",
+  "field_database",
+  "field_mailing",
+  "field_search",
+  "field_cdn",
+  "field_infra",
+  "field_docker",
+  "field_hosting",
+  "field_deps",
+  "field_ci_cd",
+];
 
 const SingleProjectCard = () => {
-    const location = useLocation();
-    const [customerContact, setCustomerContact] = useState();
-    const [customer, setCustomer] = useState();
-    const [developer, setDeveloper] = useState();
-    const [expand, setExpand ] = useState(false);
-    const nid = location.state.nid[0].value;
-    const projectName = location.state.title[0].value;
-    const customerContactNumber = location.state.field_customer_conctact[0].target_id;
-    const devNumber = location.state.field_developers[0].target_id;
-    const customerNID = location.state.field_customers[0].target_id;
-    //const data = location.state;
-    //console.log(customerNID);
+  const location = useLocation();
+  const [customerContact, setCustomerContact] = useState();
+  const [customer, setCustomer] = useState();
+  const [developer, setDeveloper] = useState();
+  const [expand, setExpand] = useState(false);
+  const nid = location.state.nid[0].value;
+  const projectName = location.state.title[0].value;
+  const customerContactNumber =
+    location.state.field_customer_conctact[0].target_id;
+  const devNumber = location.state.field_developers[0].target_id;
+  const customerNID = location.state.field_customers[0].target_id;
 
-    useEffect(() => {
-        getData()
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const data = location.state;
+  const data = location.state;
 
-    const getData = () => {
-        ajaxGet("/admin/people/users").then(response => { 
-            const data1 = response.filter(u=>{
-                return u.uid[0].value === customerContactNumber;
-            }); 
-            setCustomerContact(data1[0].name[0].value);
-            const data2 = response.filter(u=>{
-                return u.uid[0].value === devNumber;
-            });
-            setDeveloper(data2[0].name[0].value);
-        });
-    
-        ajaxGet("/node/customers").then(response => { 
-            const data = response.filter(c=>{
-                return c.nid[0].value === customerNID;
-            });
-            setCustomer(data[0].title[0].value);
-        });
-    };
+  const getData = () => {
+    ajaxGet("/admin/people/users").then((response) => {
+      const data1 = response.filter((u) => {
+        return u.uid[0].value === customerContactNumber;
+      });
+      setCustomerContact(data1[0].name[0].value);
+      const data2 = response.filter((u) => {
+        return u.uid[0].value === devNumber;
+      });
+      setDeveloper(data2[0].name[0].value);
+    });
 
-    return (
-    <div >
-        <div >
-          <div >
-            <h3>"{ projectName }"</h3>
-            <div>Customer: {customer}</div>
-            <div>Customer contact: {customerContact}</div>
-            <div>Developer: {developer}</div>
-          </div>
-          <div >
-            <button onClick={()=>setExpand(!expand)} className="infoButton">{expand ?  "Less info": "More info"}</button>
-              {
-                expand &&
-                servicesKey.map((s,i) => (
-                  <div key={i} style={{
-                      display:"flex",
-                      justifyContent:"space-between",
+    ajaxGet("/node/customers").then((response) => {
+      const data = response.filter((c) => {
+        return c.nid[0].value === customerNID;
+      });
+      setCustomer(data[0].title[0].value);
+    });
+  };
 
-                    }}>
-                      <label> {capitalStart(s.split("_")[1])}: </label>
-                      <p>{data[s][0]?.value}</p>
-                  </div>
-                ))
-              }
-          </div>
+  return (
+    <div>
+      <div className="singleProjectContainer">
+        <h2>{projectName}</h2>
+        <div className="singleProjectDiv">Customer: {customer}</div>
+        <div className="singleProjectDiv">
+          Customer contact: {customerContact}
+        </div>
+        <div className="singleProjectDiv">Developer: {developer}</div>
+      </div>
+      <div>
+        <button onClick={() => setExpand(!expand)} className="infoButton">
+          {expand ? "Less info" : "More info"}
+        </button>
+        <div className="singleExtraInfo">
+          {expand &&
+            servicesKey.map((s, i) => (
+              <div key={i}>
+                <label className="servicesKeys">
+                  {" "}
+                  {capitalStart(s.split("_")[1])}:{" "}
+                </label>
+                <p className="servicesValues">{data[s][0]?.value}</p>
+              </div>
+            ))}
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default SingleProjectCard;
